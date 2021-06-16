7ff3e088269ba41b4548eef0ba2e944f61739874
Shotaro Baba <bshotaro@gmail.com>
2021-06-16 16:05:45 +0200

    Removed unnecessary HTML tags

--- www/.plugins/header.php
+++ www/.plugins/header.php
@@ -3,13 +3,7 @@
   top: 0;
   width: 100%;
 ">
-  <!-- <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-    <button class="navbar-toggler ms-auto" type="button" data-mdb-toggle="collapse"
-        data-mdb-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <img src='/pics/list.svg' height='16' width='16'></img>
-    </button>
-  </a> -->
+ 
   <button type="button" class="navbar-toggler ms-auto" data-toggle="collapse" data-target="#navbarSupportedContent">
     <span class="sr-only"><img src='/pics/list.svg' height='16' width='16'></img></span>
   </button>&nbsp;&nbsp;
@@ -26,17 +20,11 @@
         <a class="nav-link" href="/contact">&nbsp;Contact</a>
       </li>
     </ul>
-    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+
     
   </div>
 
   
 </nav>
-<div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-light shadow-3 p-4">
-      <button class="btn btn-link btn-block border-bottom m-0">Link 1</button>
-      <button class="btn btn-link btn-block border-bottom m-0">Link 2</button>
-      <button class="btn btn-link btn-block m-0">Link 3</button>
-    </div>
-</div>
+
   
